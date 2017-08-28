c05f02edf728eee10a01fde81708e5b76d8eb959
GGZZHH <471992509@qq.com>
2017-08-28 15:40:22 +0800

    hello world!

---
+++ README.md
@@ -0,0 +1,14 @@
+学习狗书产生的代码
+================
+
+* {{ 变量|过滤器 }}
+    * 如  ```Hello, {{ name|capitalize }} ```把name变为首字母大写的方式展现出来
+
+* 控制结构
+    ```  {% if user %}
+            Hello, {{ user }}!
+         {% else %}
+            Hello, Stranger!
+         {% endif %}
+    ```
+
--- hello.py
+++ hello.py
@@ -1,15 +1,39 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, request
+from flask import Flask, request, render_template
+# 设置一个响应对象
+from flask import make_response
+# 重定向, abort终止（用于处理错误）
+from flask import redirect, abort
+# flask拓展 flask-script
+from flask_script import Manager
+
 app = Flask(__name__)
+manager = Manager(app)
 
 @app.route('/')
 def index():
-    user_agent = request.headers.get('User-Agent')
-    return "<h1>Hello World! 你使用的浏览器是：%s</h1>" % user_agent
+    # 获取首部user-agent（浏览器信息）
+    # user_agent = request.headers.get('User-Agent')
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s!<h1>' % name
+    return render_template('user.html', name=name)
+
+@app.route('/response')
+def res():
+    response = make_response('<h1>这个对象运输一个cookie！</h1>')
+    response.set_cookie('answer', '42')
+    return response
+
+@app.route('/redirect/<id>')
+def redirect_to_baidu(id):
+    user = id
+    if not user:
+        abort(404)
+    # return redirect('https://www.baidu.com')
+    return id
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    manager.run()
