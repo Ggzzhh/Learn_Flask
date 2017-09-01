5aedc013d785b75bde74fb746ac241ec72fe16a7
GGZZHH <471992509@qq.com>
2017-09-01 15:38:47 +0800

    hello Flask!

--- hello.py
+++ hello.py
@@ -1,23 +1,43 @@
 # -*- coding: utf-8 -*-
+# 导入Flask， 返回内容， 提供的模版
 from flask import Flask, request, render_template
-# 设置一个响应对象
-from flask import make_response
+# 设置一个响应对象, 用户对话， 地址, flash
+from flask import make_response, session, url_for, flash
 # 重定向, abort终止（用于处理错误）
 from flask import redirect, abort
 # flask拓展 flask-script
 from flask_script import Manager
 # flask拓展 flask-bootstrap
 from flask_bootstrap import Bootstrap
+# flask拓展 flask-wtf
+from flask_wtf import FlaskForm
+# stringField 就是type=text的input  submit同理
+from wtforms import StringField, SubmitField
+# 导入验证器 的要求
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+# 设置密匙
+app.config['SECRET_KEY'] = '!(()(!$'
 
-@app.route('/')
+class NameForm(FlaskForm):
+    name = StringField('你的名字是？', validators=[DataRequired()])
+    submit = SubmitField('submit')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     # 获取首部user-agent（浏览器信息）
     # user_agent = request.headers.get('User-Agent')
-    return render_template('index.html')
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('你的名字变成了' + form.name.data)
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
